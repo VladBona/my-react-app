a27407aea800cdbbc78d7469092fc14594d10d9c
Vladyslav Banasko <vlad.bona@gmail.com>
2020-06-21 17:04:41 -0400

    css updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
     <div>
-        <h1>LEarn to design and code React apps</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <Link to="/page-2/">watch the video1</Link>
+        <div className="Hero">
+            <div className="HeroGroup">
+                <h1>Learn to design and code React apps</h1>
+                <p>Welcome to your new Gatsby site.</p>
+                <Link to="/page-2/">watch the video1</Link>
+            </div>
+        </div>
     </div>
 )
 
